929dfc21cf72477cfbed570f58100f4e9ddb2165
David Oladapo <ibksmart2004@gmail.com>
2024-08-25 23:04:21 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
-from .models import Book
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
 
-#def book_list(request):
-#    books = Book.objects.all()
-#    book_info = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-#    return HttpResponse(book_info, content_type="text/plain")
-
+# Function-based view to list all books
 def book_list(request):
     books = Book.objects.all()
     context = {
@@ -13,11 +10,34 @@ def book_list(request):
     }
     return render(request, 'relationship_app/book_list.html', context)
 
-from django.views.generic import DetailView
-from .models import Library
-
+# Class-based view to display details for a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+    
+################
+
+##from django.http import HttpResponse
+##from .models import Book
+
+#def book_list(request):
+#    books = Book.objects.all()
+#    book_info = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+#    return HttpResponse(book_info, content_type="text/plain")
+
+##def book_list(request):
+##    books = Book.objects.all()
+##    context = {
+##        'books': books
+##    }
+##    return render(request, 'relationship_app/book_list.html', context)
+
+##from django.views.generic import DetailView
+##from .models import Library
+
+##class LibraryDetailView(DetailView):
+##    model = Library
+##    template_name = "relationship_app/library_detail.html"
+##    context_object_name = "library"
 
