3d315a7f7a90e32d6fc483ab58e5b3111366ad17
David Oladapo <ibksmart2004@gmail.com>
2024-08-25 08:46:57 +0100

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -34,3 +34,21 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+from django.db import models
+
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    class Meta:
+        permissions = [
+            ('can_view', 'Can view document'),
+            ('can_create', 'Can create document'),
+            ('can_edit', 'Can edit document'),
+            ('can_delete', 'Can delete document'),
+        ]
+
+    def __str__(self):
+        return self.title
+
