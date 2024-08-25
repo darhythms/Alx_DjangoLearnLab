5dd40490ba732cb69e1519df4b4c2ff58e052704
David Oladapo <ibksmart2004@gmail.com>
2024-08-25 09:13:54 +0100

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,27 @@
+from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(
+        max_length=100, 
+        required=True, 
+        widget=forms.TextInput(attrs={
+            'class': 'form-control', 
+            'placeholder': 'Enter your name'
+        })
+    )
+    email = forms.EmailField(
+        required=True, 
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control', 
+            'placeholder': 'Enter your email'
+        })
+    )
+    message = forms.CharField(
+        required=True, 
+        widget=forms.Textarea(attrs={
+            'class': 'form-control', 
+            'placeholder': 'Enter your message',
+            'rows': 5
+        })
+    )
 
