a8affb2515fbdd46f26c0dda14f77d292e6a34fc
David Oladapo <ibksmart2004@gmail.com>
2024-08-25 08:28:28 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,3 +5,12 @@ def book_list(request):
     books = Book.objects.all()
     book_info = "\n".join([f"{book.title} by {book.author.name}" for book in books])
     return HttpResponse(book_info, content_type="text/plain")
+
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
+
