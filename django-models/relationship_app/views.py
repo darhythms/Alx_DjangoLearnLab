0e775970a259aca7b7d6ab00e504655238318899
David Oladapo <ibksmart2004@gmail.com>
2024-08-25 22:56:26 +0100

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from .models import Book
 
+#def book_list(request):
+#    books = Book.objects.all()
+#    book_info = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+#    return HttpResponse(book_info, content_type="text/plain")
+
 def book_list(request):
     books = Book.objects.all()
-    book_info = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(book_info, content_type="text/plain")
+    context = {
+        'books': books
+    }
+    return render(request, 'relationship_app/book_list.html', context)
 
 from django.views.generic import DetailView
 from .models import Library
