386c6d8d1fbda6dfa2ef9dfb4795a7521c13eaa5
David Oladapo <ibksmart2004@gmail.com>
2024-09-01 21:41:15 +0100

    Create settings.py

---
+++ api_project/settings.py
@@ -0,0 +1,10 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',  # Ensure this line is present
+    'api',             # Ensure this line is present
+]
