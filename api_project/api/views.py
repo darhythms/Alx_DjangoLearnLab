0c1bde2a7ca96373debfba782365424fb0c63371
David Oladapo <ibksmart2004@gmail.com>
2024-09-01 21:48:06 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 # api/views.py
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView  # Ensure this import is correct
 from .models import Book
 from .serializers import BookSerializer
 
